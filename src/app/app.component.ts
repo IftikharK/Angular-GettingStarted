ff581f9530e88be34ab58a509a70c8adf9dd3b1e
Iftikhar Ahmad Khan <iftikharkhan@kpmg.com>
2020-04-13 17:14:35 +0200

    product list component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <h1>{{ title }}</h1>
+      <app-product-list></app-product-list>
+    </div>
+  `,
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Acme Product Management';
