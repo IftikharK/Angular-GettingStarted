9b9fe10db4e831659c2a61a8cb2c9ca74a9a96be
iftikharkhan <iftikharkhan@kpmg.com>
2020-04-20 17:34:08 +0200

    modules organized in a better way

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list/product-list.component';
-import { ConvertToSpacesPipe } from './shared/pipes/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star/star.component';
-import { from } from 'rxjs';
-import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { WelcomeComponent } from './home/welcome/welcome.component';
-import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products/product-detail/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
-    WelcomeComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent }, // navigate to product list
-      { path: 'products/:id', canActivate: [ProductDetailGuard] , component: ProductDetailComponent }, // protecting route with guards
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' }, // defalt routes -> welcome component
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }, // wild card path incase the requested url doesn't match any path
-    ]),
+    ProductModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent] // identify root component
 })
 export class AppModule { }
